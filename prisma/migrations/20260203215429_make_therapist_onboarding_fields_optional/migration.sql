cd275f6fc17f6023b770b092f9d286b9b6731f15
steveZavi <stevezavi9@gmail.com>
2026-02-11 17:34:07 +0300

    Reiniatialized the schema

--- prisma/migrations/20260203215429_make_therapist_onboarding_fields_optional/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "therapist_profiles" ALTER COLUMN "specialization" DROP NOT NULL,
-ALTER COLUMN "license_number" DROP NOT NULL;
--- prisma/migrations/20260204043726_make_location_optional/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "customer_profiles" ALTER COLUMN "location" DROP NOT NULL;
