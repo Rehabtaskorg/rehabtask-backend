d74292d52f97b6c62ee1157568a899b4a230d893
steveZavi <stevezavi9@gmail.com>
2026-02-17 19:10:39 +0300

    Fix dotenv load order, correct RESEND_API_KEY validation

--- src/config/email.js
+++ src/config/email.js
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-if (process.env.RESEND_API_KEY) {
+if (!process.env.RESEND_API_KEY) {
     throw new Error("RESEND_API_KEY is not defined in environment variables");
 }
 
